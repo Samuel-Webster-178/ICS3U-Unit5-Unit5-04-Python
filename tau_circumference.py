4f0500be73805a0aaeb7e3a283c8a84fcbd6558a
Ubuntu <ubuntu@ip-172-31-84-56.ec2.internal>
2022-05-10 18:34:41 +0000

    init

--- tau_circumference.py
+++ tau_circumference.py
@@ -6,21 +6,40 @@
 #     with inputted radius
 
 
-import constants
+import math
+
+
+def CalculateVolume(radius, height):
+    # I calculate circumference
+
+    # process
+    volume = math.pi * height * radius**2
+
+    # output
+    return volume
 
 
 def main():
     # I calculate circumference
 
-    # input
-    radius = int(input("Enter radius of the circle in mm: "))
-
-    # process
-    circumference = constants.TAU * radius
-
-    # output
-    print("Circumference is {} mm.".format(circumference))
-    print("\nDone.")
+    # input & output
+    print("This program calculates the volume of a cylinder.")
+    print("Please enter radius and height.")
+    str_radius = input("\nThe radius is (mm): ")
+    str_height = input("The height is (mm): ")
+    try:
+        int_radius = int(str_radius)
+        int_height = int(str_height)
+        volume = CalculateVolume(int_radius, int_height)
+        print(
+            "\nThe volume of a cylinder with radius {0} mm and height {1} mm is {2} mm².".format(
+                int_radius, int_height, volume
+            )
+        )
+    except Exception:
+        print("Invalid Input")
+    finally:
+        print("\nDone.")
 
 
 if __name__ == "__main__":
